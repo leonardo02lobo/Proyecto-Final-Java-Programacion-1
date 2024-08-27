6c2e9a6b45d4263026079cff85e62755a9aa4f7d
leonardo lobo <loboleonardo75@gmail.com>
2024-08-27 18:46:41 -0400

    agregando el uso del cambio de personajes

--- src/GUI/Iniciar_Juego.java
+++ src/GUI/Iniciar_Juego.java
@@ -4,7 +4,7 @@ import Implementacion.*;
 import java.awt.*;
 import java.awt.event.*;
 import javax.swing.*;
-import logica.musica;
+import logica.*;
 
 public class Iniciar_Juego extends JPanel {
 
@@ -12,14 +12,21 @@ public class Iniciar_Juego extends JPanel {
         setLayout(null);
         setBackground(Color.BLACK);
 
-        crearEtiquetas(new JLabel("Space Invader", SwingConstants.CENTER), 100, 50, 300, 70, 40);
-        crearEtiquetas(etiquetaNombre, 100, 150, 200, 30, 20);
-        Cajas(nombre, 120, 200, 200, 30);
-        CrearBoton(iniciarJuego, 120, 300, 100, 30, "Iniciar");
-        CrearBoton(volver, 270, 300, 100, 30, "volver");
+        crearEtiquetas(new JLabel("Space Invader", SwingConstants.CENTER), 100, 50, 300, 70, 40,false,"");
+        crearEtiquetas(etiquetaNombre, 100, 100, 200, 30, 20,false,"");
+        Cajas(nombre, 120, 150, 200, 30);
+//        crearEtiquetas(imagenBlancoNegro, 70, 250, 100, 100, 0,false,"../source/enemigos/Calamar2.png");
+        crearEtiquetas(imagenColor, 300, 250, 100, 100, 20,true,"../source/Sprites Color/enemy1_1.png");
+        CrearBoton(iniciarJuego, 120, 420, 100, 30, "Iniciar");
+        CrearBoton(volver, 270, 420, 100, 30, "volver");
     }
 
-    private void crearEtiquetas(JLabel label, int x, int y, int ancho, int alto, int tam) {
+    private void crearEtiquetas(JLabel label, int x, int y, int ancho, int alto, int tam,boolean band,String ruta) {
+        if(band){
+            Image imagen = new ImageIcon(getClass().getResource(ruta)).getImage();
+            label.setIcon(new ImageIcon(imagen.getScaledInstance(ancho, alto, Image.SCALE_SMOOTH)));
+            label.setBorder(BorderFactory.createLineBorder(Color.white, 2));
+        }
         label.setFont(new Font("calibri", Font.BOLD, tam));
         label.setBounds(x, y, ancho, alto);
         label.setForeground(Color.white);
@@ -60,6 +67,8 @@ public class Iniciar_Juego extends JPanel {
     }
 
     private JLabel etiquetaNombre = new JLabel("Ingrese su nombre:", SwingConstants.CENTER);
+    private JLabel imagenBlancoNegro = new JLabel();
+    private JLabel imagenColor = new JLabel();
     private JTextField nombre = new JTextField();
     private JButton iniciarJuego = new JButton();
     private JButton volver = new JButton();
